877f8a6b21dc1598754ea0ba3f474931804cab6b
Davidhernandezn <d4vidhernandezn@gmail.com>
2024-01-26 14:54:12 -0600

    Actualizado de api POKEMON

--- src/main/java/com/mifel/demo/controller/PokemonController.java
+++ src/main/java/com/mifel/demo/controller/PokemonController.java
@@ -1,21 +1,33 @@
 package com.mifel.demo.controller;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 @SpringBootApplication
+@RequestMapping("/pokemon")
 public class PokemonController {
 
-    @GetMapping("/pokemon")
-    public String getPokemonData() {
-        String apiUrl = "https://pokeapi.co/api/v2/pokemon/ditto";
+    @GetMapping("/dato")
+    public String getPokemonData(@RequestParam String nombre) {
+        String apiUrl = "https://pokeapi.co/api/v2/pokemon/" + nombre;
 
         RestTemplate restTemplate = new RestTemplate();
         String jsonResponse = restTemplate.getForObject(apiUrl, String.class);
+        return jsonResponse;
+    }
 
-        // Devolver la respuesta en formato JSON
+    @GetMapping("/{nombre}")
+    public String getPokemonDataByPath(@PathVariable String nombre) {
+        String apiUrl = "https://pokeapi.co/api/v2/pokemon/" + nombre;
+
+        RestTemplate restTemplate = new RestTemplate();
+        String jsonResponse = restTemplate.getForObject(apiUrl, String.class);
         return jsonResponse;
     }
 }
